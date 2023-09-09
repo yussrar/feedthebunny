b7fd3e2fb8c0835115ad63b2c7b5ccbcc4d02ccc
yussrar <123570172+yussrar@users.noreply.github.com>
2023-09-09 00:06:40 -0400

    Add files via upload

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,5 @@
 
-window.onload= function(){
+window.onload = function () {
 
   //getting messages and buttons
   var message1 = document.getElementById("message1");
@@ -10,27 +10,34 @@ window.onload= function(){
   btn1.onclick = startGame;
   btn2.onclick = reloadPage;
 
+  var audio = document.getElementById("backAudio");
+  var audio2 = document.getElementById("myAudio");
+
+
+  //making a constant total for total bunnies
+  let total = 0;
   var counter = 0;
 
-  message2.style.display = "none";
 
+  message2.style.display = "none";
   var rabbitInterval;
 
-    //getting the counter 
-    var counterShow = document.getElementById("counter");
-    counterShow.style.display="none";
+  //getting the counter 
+  var counterShow = document.getElementById("counter");
+  counterShow.style.display = "none";
 
-  function startGame(){
+
+
+  function startGame() {
 
     //playing background music 
-    var audio = document.getElementById("backAudio");
     audio.loop = true;
     audio.play();
 
     setTimeout(stopGame, 20000);
 
-    message1.style.display="none";
-    counterShow.style.display="block";
+    message1.style.display = "none";
+    counterShow.style.display = "block";
 
     // Get the rabbits container element
     const rabbitsContainer = document.getElementById("rabbits");
@@ -39,10 +46,14 @@ window.onload= function(){
     function generateRandomPosition() {
       const containerWidth = rabbitsContainer.offsetWidth;
       const containerHeight = rabbitsContainer.offsetHeight;
-      const left = Math.floor(Math.random() * containerWidth);
-      const top = Math.floor(Math.random() * containerHeight)+740;
+      const left = Math.floor(Math.random() * (containerWidth - 20));
+      const top = Math.floor(Math.random() * containerHeight) + 740;
       return { left, top };
     }
+
+    // Call the createNewRabbit function at a fixed interval to create new rabbits
+    rabbitInterval = setInterval(createNewRabbit, 800);
+
 
     // Create a function to create a new rabbit element and add it to the container
     function createNewRabbit() {
@@ -52,39 +63,41 @@ window.onload= function(){
       rabbit.style.left = `${left}px`;
       rabbit.style.top = `${top}px`;
       rabbitsContainer.appendChild(rabbit);
+      total = total + 1
+
+      // Add a click event listener to the newly created rabbit element
+      rabbit.addEventListener("click", () => rabbitFed(rabbit));
     }
 
-    // Call the createNewRabbit function at a fixed interval to create new rabbits
-    rabbitInterval = setInterval(createNewRabbit, 700);
 
-    //adding a function that counts how many times the rabbit is clicked
-    rabbitsContainer.onclick= rabbitFed;
+
 
     //add the counter and play audio when rabbit fed
-    function rabbitFed(){
-      var audio2 = document.getElementById("myAudio");
-      var counts= document.getElementById("counterShow")
+    function rabbitFed(rabbit) {
+      rabbit.style.backgroundImage = 'url("media/bun-fed.png")'; 
+      var counts = document.getElementById("counterShow")
       audio2.play();
-      counter = counter+1;
+      counter = counter + 1;
       counts.innerHTML = counter;
       console.log(counter);
 
-      clickedRabbit.querySelector('img').src = "new-image.jpg";
+      
+      
+
+
     }
   }
-
-
-
 
   // end the game by stopping rabit intervel and display counter
   function stopGame() {
     clearInterval(rabbitInterval);
-    message2.style.display= "block";
-    counterShow.style.display="none";
-    counts.innerHTML = counter;
+    message2.style.display = "block";
+    counterShow.style.display = "none";
+    counts.innerHTML = counter + " out of " + total;
+    audio.pause();
   }
 
-  function reloadPage(){
+  function reloadPage() {
     location.reload();
   }
 }
